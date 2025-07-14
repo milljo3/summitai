deda6e804ed9edc57b6c75276cf5be3ef66229c9
joey102522 <joeykm91@gmail.com>
2025-07-13 20:21:25 -0400

    fix: made meeting cards a link + tag display correct

--- src/components/meeting/MeetingCard.tsx
+++ src/components/meeting/MeetingCard.tsx
@@ -1,4 +1,5 @@
 import {MeetingCard as MeetingCardType} from "@/types/meeting";
+import Link from "next/link";
 
 interface MeetingCardProps {
     meetingCard: MeetingCardType;
@@ -6,21 +7,22 @@ interface MeetingCardProps {
 
 export const MeetingCard = ({meetingCard}: MeetingCardProps) => {
     return (
-        <div className="flex flex-wrap justify-center gap-4">
-            <div className="flex flex-col items-center justify-center p-5 gap-2 border-2 border-black">
-                <h1 className="text-lg">{meetingCard.title}</h1>
-                <p>{meetingCard.summary}</p>
-                <div className="flex flex-wrap gap-2">
-                    {meetingCard.tags.map(tag => (
-                        <div
-                            key={tag}
-                            className="flex items-center justify-center border-2 border-black bg-blue-300 rounded-md"
-                        >
-                            <p>tag</p>
-                        </div>
-                    ))}
-                </div>
+        <Link
+            href={`/meetings/${meetingCard.id}`}
+            className="flex flex-col items-center p-5 gap-2 border-2 border-black w-[300px] h-64 overflow-y-auto rounded-md"
+        >
+            <h1 className="text-lg">{meetingCard.title}</h1>
+            <p>{meetingCard.summary}</p>
+            <div className="flex flex-wrap gap-2">
+                {meetingCard.tags.map(tag => (
+                    <div
+                        key={tag}
+                        className="flex items-center justify-center border-2 border-black bg-blue-300 rounded-md px-2"
+                    >
+                        <p>{tag}</p>
+                    </div>
+                ))}
             </div>
-        </div>
+        </Link>
     );
 };
