1af2ada91d644a9b746b85fd2ec9c4d4e332f7b3
joey102522 <joeykm91@gmail.com>
2025-07-14 19:01:52 -0400

    feat: made meeting cards collapsible
    only shows 3 tags, can click view more to see all tags

--- src/components/meetings/MeetingCard.tsx
+++ src/components/meetings/MeetingCard.tsx
@@ -1,14 +1,30 @@
 import {MeetingCard as MeetingCardType} from "@/types/meeting";
 import Link from "next/link";
 import {ArrowUpRight} from "lucide-react";
+import { Button } from "@/components/ui/button"
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {useState} from "react";
 
 interface MeetingCardProps {
     meetingCard: MeetingCardType;
 }
 
+const INITIAL_TAGS = 3;
+
 export const MeetingCard = ({meetingCard}: MeetingCardProps) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const initialTags = meetingCard.tags.slice(0, INITIAL_TAGS);
+    const remainingTags = meetingCard.tags.slice(INITIAL_TAGS);
+
     return (
-        <div
+        <Collapsible
+            open={isOpen}
+            onOpenChange={setIsOpen}
             className="flex flex-col items-center p-5 gap-2 border-2 border-black w-[300px] h-64 overflow-y-auto rounded-md"
         >
             <Link
@@ -18,16 +34,35 @@ export const MeetingCard = ({meetingCard}: MeetingCardProps) => {
                 {meetingCard.title} <ArrowUpRight />
             </Link>
             <p>{meetingCard.summary}</p>
-            <div className="flex flex-wrap gap-2">
-                {meetingCard.tags.map(tag => (
-                    <div
-                        key={tag}
-                        className="flex items-center justify-center border-2 border-black bg-blue-300 rounded-md px-2"
-                    >
-                        <p>{tag}</p>
-                    </div>
-                ))}
+            <div className="flex flex-col gap-2">
+                <div className="flex flex-wrap gap-2">
+                    {initialTags.map(tag => (
+                        <div
+                            key={tag}
+                            className="flex items-center justify-center border-2 border-black bg-blue-300 rounded-md px-2"
+                        >
+                            <p>{tag}</p>
+                        </div>
+                    ))}
+                </div>
+                {remainingTags.length > 0 && (
+                    <CollapsibleContent className="flex flex-wrap gap-2">
+                        {remainingTags.map(tag => (
+                            <div
+                                key={tag}
+                                className="flex items-center justify-center border-2 border-black bg-blue-300 rounded-md px-2"
+                            >
+                                <p>{tag}</p>
+                            </div>
+                        ))}
+                    </CollapsibleContent>
+                )}
             </div>
-        </div>
+            <CollapsibleTrigger asChild>
+                <Button size="default" className="w-1/2" variant="ghost">
+                    <p>{isOpen ? "View less" : "View more"}</p>
+                </Button>
+            </CollapsibleTrigger>
+        </Collapsible>
     );
 };
