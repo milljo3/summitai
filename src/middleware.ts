33f9cc7e5167151a6df11483029ec64b82342f47
joey102522 <joeykm91@gmail.com>
2025-07-14 18:22:11 -0400

    feat: added meetings to basic middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {getSessionCookie} from "better-auth/cookies";
 
-const protectedRoutes = ["/dashboard"];
+const protectedRoutes = ["/dashboard", "/meetings"];
 
 export async function middleware(req: NextRequest) {
     const {nextUrl} = req;
