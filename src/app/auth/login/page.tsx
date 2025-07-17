ea6b0edb4122f6a6b119a15616e502148cfd0668
joey102522 <joeykm91@gmail.com>
2025-07-17 16:25:49 -0400

    style: added site name

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -6,7 +6,8 @@ function Page() {
         <div className="flex items-center justify-center h-dvh">
             <div className="px-8 py-16 container mx-auto max-w-screen-lg space-y-8 flex items-center justify-center w-full">
                 <div className="space-y-8 w-full self-center">
-                    <div className="space-y-8 text-center">
+                    <div className="space-y-6 text-center">
+                        <h1 className="text-6xl font-bold">SummitAi</h1>
                         <h1 className="text-3xl font-bold">Login</h1>
                     </div>
                     <div className="flex flex-col items-center justify-center gap-4">
